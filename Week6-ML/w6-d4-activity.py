8a1371cf630f7f0747934ec26fa5494b47e34f8e
Salish Shakya <salishakya@gmail.com>
2024-09-22 10:11:03 +0545

    multiple linear regression activity assignment

--- Week6-ML/w6-d4-activity.py
+++ Week6-ML/w6-d4-activity.py
@@ -1,3 +1,64 @@
 # Annotated follow-along guide_ Interpret multiple regression results with Python
 
 # multicolinearity
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv("./csv/marketing_sales.csv")
+data.head()
+data.columns
+
+# • TV promotional budget (in “Low,” “Medium,” and “High” categories)
+# • Social media promotional budget (in millions of dollars)
+# • Radio promotional budget (in millions of dollars)
+# • Sales (in millions of dollars)
+# • Influencer size (in “Mega,” “Macro,” “Micro,” and “Nano” categories)
+
+sns.pairplot(data=data)
+
+tv_sales = data[["TV", "Sales"]]
+tv_sales_mean = tv_sales["Sales"].mean()
+
+influencer_sales = data[["Influencer", "Sales"]]
+influencer_sales_mean = influencer_sales["Sales"].mean()
+
+data.groupby("TV")["Sales"].mean()
+data.groupby("Influencer")["Sales"].mean()
+
+data.dropna(inplace=True)
+data.reset_index(inplace=True, drop=True)
+
+data.columns
+
+data.rename(columns={"Social Media": "Social_Media"}, inplace=True)
+
+# Define the OLS formula.
+# Create an OLS model.
+# Fit the model.
+# Save the results summary.
+# Display the model results.
+
+data_X = data[["TV", "Radio", "Social_Media", "Influencer"]]
+data_y = data[["Sales"]]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    data_X, data_y, test_size=0.3, random_state=42
+)
+
+ols_formula = "Sales ~ Radio + Social_Media + C(TV) + C(Influencer)"
+
+ols_data = pd.concat([X_train, y_train], axis=1)
+
+ols_data.iloc[1]
+data.iloc[1]
+
+from statsmodels.formula.api import ols
+
+OLS = ols(formula=ols_formula, data=ols_data)
+model = OLS.fit()
+
+model.summary()
